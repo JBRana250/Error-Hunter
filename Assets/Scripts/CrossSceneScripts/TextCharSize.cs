267f65b124b0e8c69d2bf2b4304b5ab9fe4d9ec2
JB Rana <JBRana2500@gmail.com>
2023-10-23 08:59:08 +1300

    added grade system to score screen

--- Assets/Scripts/CrossSceneScripts/TextCharSize.cs
+++ Assets/Scripts/CrossSceneScripts/TextCharSize.cs
@@ -1,16 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// this script contains the default size of all the characters when font size = 1. (+ maybe other related info?)
-// it will be static in case I will need to use it across scenes (ex change size of text on main menu screen)
-
 static class TextCharSize
 {
-    //the information will be contained primarily in a dictionary, in a string : object format, where the string acts as the reference variable for each character object,
-    // and I will just instantiate the character objects without a reference variable.
-
-    //im having this be a class so each 'character' object can have multiple 'size' attributes, incase I need it in the future (ex diff fonts?)
+    // The Character class remains the same
     public class Character
     {
         public Vector2 charSize;
@@ -21,91 +14,89 @@ static class TextCharSize
         }
     }
 
-    public static Dictionary<string, Character> Characters = new Dictionary<string, Character>();
-
-    //go through all characters that I may need and add them to the dictionary
-    public static void AddCharacters()
+    // Initialize the dictionary with character objects. This is initialized with the elements already inside so I dont have to use add method anymore.
+    public static Dictionary<string, Character> Characters = new Dictionary<string, Character>
     {
-        Characters.Add("a", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("b", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("c", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("d", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("e", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("f", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("g", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("h", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("i", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("j", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("k", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("l", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("m", new Character(new Vector2(0.09f, 0.12f)));
-        Characters.Add("n", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("o", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("p", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("q", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("r", new Character(new Vector2(0.04f, 0.12f)));
-        Characters.Add("s", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("t", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("u", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("v", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("w", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("x", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("y", new Character(new Vector2(0.05f, 0.12f)));
-        Characters.Add("z", new Character(new Vector2(0.05f, 0.12f)));
-        Characters.Add("A", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("B", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("C", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("D", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("E", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("F", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("G", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("H", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("I", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("J", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("K", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("L", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("M", new Character(new Vector2(0.09f, 0.12f)));
-        Characters.Add("N", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("O", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("P", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("Q", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("R", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("S", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("T", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("U", new Character(new Vector2(0.08f, 0.12f)));
-        Characters.Add("V", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("W", new Character(new Vector2(0.10f, 0.12f)));
-        Characters.Add("X", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("Y", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("Z", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("1", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("2", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("3", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("4", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("5", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("6", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("7", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("8", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("9", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("0", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("!", new Character(new Vector2(0.04f, 0.12f)));
-        Characters.Add("@", new Character(new Vector2(0.11f, 0.12f)));
-        Characters.Add("#", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("$", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("%", new Character(new Vector2(0.09f, 0.12f)));
-        Characters.Add("^", new Character(new Vector2(0.05f, 0.12f)));
-        Characters.Add("&", new Character(new Vector2(0.07f, 0.12f)));
-        Characters.Add("*", new Character(new Vector2(0.04f, 0.12f)));
-        Characters.Add("(", new Character(new Vector2(0.04f, 0.12f)));
-        Characters.Add(")", new Character(new Vector2(0.04f, 0.12f)));
-        Characters.Add(" ", new Character(new Vector2(0.02f, 0.12f)));
-        Characters.Add("\"", new Character(new Vector2(0.04f, 0.12f)));
-        Characters.Add("=", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add(":", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("[", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("]", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add(",", new Character(new Vector2(0.03f, 0.12f)));
-        Characters.Add("+", new Character(new Vector2(0.06f, 0.12f)));
-        Characters.Add("-", new Character(new Vector2(0.04f, 0.12f)));
-    }
+        { "a", new Character(new Vector2(0.06f, 0.12f)) },
+        { "b", new Character(new Vector2(0.06f, 0.12f)) },
+        { "c", new Character(new Vector2(0.06f, 0.12f)) },
+        { "d", new Character(new Vector2(0.06f, 0.12f)) },
+        { "e", new Character(new Vector2(0.06f, 0.12f)) },
+        { "f", new Character(new Vector2(0.03f, 0.12f)) },
+        { "g", new Character(new Vector2(0.06f, 0.12f)) },
+        { "h", new Character(new Vector2(0.06f, 0.12f)) },
+        { "i", new Character(new Vector2(0.03f, 0.12f)) },
+        { "j", new Character(new Vector2(0.03f, 0.12f)) },
+        { "k", new Character(new Vector2(0.06f, 0.12f)) },
+        { "l", new Character(new Vector2(0.03f, 0.12f)) },
+        { "m", new Character(new Vector2(0.09f, 0.12f)) },
+        { "n", new Character(new Vector2(0.06f, 0.12f)) },
+        { "o", new Character(new Vector2(0.06f, 0.12f)) },
+        { "p", new Character(new Vector2(0.06f, 0.12f)) },
+        { "q", new Character(new Vector2(0.06f, 0.12f)) },
+        { "r", new Character(new Vector2(0.04f, 0.12f)) },
+        { "s", new Character(new Vector2(0.06f, 0.12f)) },
+        { "t", new Character(new Vector2(0.03f, 0.12f)) },
+        { "u", new Character(new Vector2(0.06f, 0.12f)) },
+        { "v", new Character(new Vector2(0.06f, 0.12f)) },
+        { "w", new Character(new Vector2(0.08f, 0.12f)) },
+        { "x", new Character(new Vector2(0.06f, 0.12f)) },
+        { "y", new Character(new Vector2(0.05f, 0.12f)) },
+        { "z", new Character(new Vector2(0.05f, 0.12f)) },
+        { "A", new Character(new Vector2(0.07f, 0.12f)) },
+        { "B", new Character(new Vector2(0.07f, 0.12f)) },
+        { "C", new Character(new Vector2(0.08f, 0.12f)) },
+        { "D", new Character(new Vector2(0.08f, 0.12f)) },
+        { "E", new Character(new Vector2(0.07f, 0.12f)) },
+        { "F", new Character(new Vector2(0.07f, 0.12f)) },
+        { "G", new Character(new Vector2(0.08f, 0.12f)) },
+        { "H", new Character(new Vector2(0.08f, 0.12f)) },
+        { "I", new Character(new Vector2(0.03f, 0.12f)) },
+        { "J", new Character(new Vector2(0.06f, 0.12f)) },
+        { "K", new Character(new Vector2(0.07f, 0.12f)) },
+        { "L", new Character(new Vector2(0.06f, 0.12f)) },
+        { "M", new Character(new Vector2(0.09f, 0.12f)) },
+        { "N", new Character(new Vector2(0.08f, 0.12f)) },
+        { "O", new Character(new Vector2(0.08f, 0.12f)) },
+        { "P", new Character(new Vector2(0.07f, 0.12f)) },
+        { "Q", new Character(new Vector2(0.08f, 0.12f)) },
+        { "R", new Character(new Vector2(0.08f, 0.12f)) },
+        { "S", new Character(new Vector2(0.07f, 0.12f)) },
+        { "T", new Character(new Vector2(0.07f, 0.12f)) },
+        { "U", new Character(new Vector2(0.08f, 0.12f)) },
+        { "V", new Character(new Vector2(0.07f, 0.12f)) },
+        { "W", new Character(new Vector2(0.10f, 0.12f)) },
+        { "X", new Character(new Vector2(0.07f, 0.12f)) },
+        { "Y", new Character(new Vector2(0.07f, 0.12f)) },
+        { "Z", new Character(new Vector2(0.07f, 0.12f)) },
+        { "1", new Character(new Vector2(0.06f, 0.12f)) },
+        { "2", new Character(new Vector2(0.06f, 0.12f)) },
+        { "3", new Character(new Vector2(0.06f, 0.12f)) },
+        { "4", new Character(new Vector2(0.06f, 0.12f)) },
+        { "5", new Character(new Vector2(0.06f, 0.12f)) },
+        { "6", new Character(new Vector2(0.06f, 0.12f)) },
+        { "7", new Character(new Vector2(0.06f, 0.12f)) },
+        { "8", new Character(new Vector2(0.06f, 0.12f)) },
+        { "9", new Character(new Vector2(0.06f, 0.12f)) },
+        { "0", new Character(new Vector2(0.06f, 0.12f)) },
+        { "!", new Character(new Vector2(0.04f, 0.12f)) },
+        { "@", new Character(new Vector2(0.11f, 0.12f)) },
+        { "#", new Character(new Vector2(0.06f, 0.12f)) },
+        { "$", new Character(new Vector2(0.06f, 0.12f)) },
+        { "%", new Character(new Vector2(0.09f, 0.12f)) },
+        { "^", new Character(new Vector2(0.05f, 0.12f)) },
+        { "&", new Character(new Vector2(0.07f, 0.12f)) },
+        { "*", new Character(new Vector2(0.04f, 0.12f)) },
+        { "(", new Character(new Vector2(0.04f, 0.12f)) },
+        { ")", new Character(new Vector2(0.04f, 0.12f)) },
+        { " ", new Character(new Vector2(0.02f, 0.12f)) },
+        { "\"", new Character(new Vector2(0.04f, 0.12f)) },
+        { "=", new Character(new Vector2(0.06f, 0.12f)) },
+        { ":", new Character(new Vector2(0.03f, 0.12f)) },
+        { "[", new Character(new Vector2(0.03f, 0.12f)) },
+        { "]", new Character(new Vector2(0.03f, 0.12f)) },
+        { ",", new Character(new Vector2(0.03f, 0.12f)) },
+        { "+", new Character(new Vector2(0.06f, 0.12f)) },
+        { "-", new Character(new Vector2(0.04f, 0.12f)) }
+    };
 }
--- Assets/Scripts/ScoreScreenScripts/ScoreDisplayScript.cs
+++ Assets/Scripts/ScoreScreenScripts/ScoreDisplayScript.cs
@@ -3,11 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+// this script will handle both score and the grade since both are related.
+
 public class ScoreDisplayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //dictionary containing all grades and corrosponding score range. Initialize dictionary with values.
+    private Dictionary<string,List<int>> gradeBoundaries = new Dictionary<string,List<int>>
+    {
+        { "F-", new List<int>{0, 5} },
+        { "F", new List<int>{6, 10} },
+        { "F+", new List<int>{11, 15} },
+        { "D-", new List<int>{16, 25} },
+        { "D", new List<int>{26, 35} },
+        { "D+", new List<int>{36, 45} },
+        { "C-", new List<int>{46, 60} },
+        { "C", new List<int>{61, 75} },
+        { "C+", new List<int>{76, 100} },
+        { "B-", new List<int>{101, 125} },
+        { "B", new List<int>{126, 150} },
+        { "B+", new List<int>{151, 175} },
+        { "B++", new List<int>{176, 200} },
+        { "A-", new List<int>{201, 300} },
+        { "A", new List<int>{301, 400} },
+        { "A+", new List<int>{401, 500} },
+        { "A++", new List<int>{501, 750} },
+        { "S--", new List<int>{751, 1000} },
+        { "S-", new List<int>{1001, 1250} },
+        { "S", new List<int>{1251, 1500} },
+        { "S+", new List<int>{1501, 2000} },
+        { "S++", new List<int>{2001, 2500} },
+        { "SS", new List<int>{2501, 10000} } 
+    };
+    private string Score;
+    public GameObject gradeDisplayTextGO;
+    private int parsedScore;
+
+
+    
     void Start()
     {
-        gameObject.GetComponent<TextMeshProUGUI>().text = string.Concat("Score: ", CrossSceneManager.CrossSceneScore);
+        Score = CrossSceneManager.CrossSceneScore;
+        gameObject.GetComponent<TextMeshProUGUI>().text = string.Concat("Score: ", Score);
+        bool success = int.TryParse(Score, out parsedScore);
+        if(success)
+        {
+            gradeDisplayTextGO.GetComponent<TextMeshProUGUI>().text = DetermineGrade(parsedScore);
+        }
+    }
+
+    // based on an integer score value, iterate through every key-value pair in the dictionary. Get the grades and the boundaries associated with them. Then, check if the score is within the bounds. If so, return the grade. If not, keep iterating.
+    string DetermineGrade(int Score)
+    {
+        foreach(var GradeBoundaries in gradeBoundaries)
+        {
+            string grade = GradeBoundaries.Key;
+            List<int> boundaries = GradeBoundaries.Value;
+
+            int lowerBound = boundaries[0];
+            int upperBound = boundaries[1];
+
+            if(Score >= lowerBound && Score <= upperBound)
+            {
+                return grade;
+            }
+        }
+        return "How?!?";
     }
 }
